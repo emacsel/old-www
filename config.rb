627a686e9dbd8f5af6a103493e2cc63c22601511
Mica Semrick <mica@silentumbrella.com>
2016-02-27 14:15:35 -0800

    Uncommented middleman-deploy in config.rb. Use the command  to build and deploy the site to github pages.

--- config.rb
+++ config.rb
@@ -126,11 +126,7 @@ configure :build do
   # set :http_prefix, "/Content/images/"
 end
 
-# activate :deploy do |deploy|
-#   deploy.deploy_method = :git
-#   # Optional Settings
-#   # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
-#   # deploy.branch   = 'custom-branch' # default: gh-pages
-#   # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
-#   # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
-# end
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.build_before = true
+end
